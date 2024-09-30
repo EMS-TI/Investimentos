9ec44adf3fccc6f87f19f50d0ffa46e4715aa654
EMS-TI <ememail@hotmail.com>
2024-09-30 01:08:09 +0000

    index.html

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+    e.preventDefault(); // Evita o envio do formulário
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    const rememberMe = document.getElementById('rememberMe').checked;
     const errorMessage = document.getElementById('errorMessage');
 
-    // Simulando validação de login
+    // Simulação de login simples
     if (username === 'admin' && password === '1234') {
-        // Redirecionar para a página de depósitos
+        // Redireciona para a página de depósitos
         window.location.href = 'depositos.html';
     } else {
+        // Exibe mensagem de erro se as credenciais forem inválidas
         errorMessage.style.display = 'block';
         errorMessage.innerText = 'Nome de usuário ou senha incorretos';
     }
 });
 
-// Mostrar/ocultar senha
+// Mostrar ou ocultar a senha
 document.getElementById('togglePassword').addEventListener('click', function() {
     const passwordField = document.getElementById('password');
     if (passwordField.type === 'password') {
